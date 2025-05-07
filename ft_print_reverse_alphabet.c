ce7141cc4db31bab575bc711f0de848f8a9f84a2
Spiritpanther08 <harshnkpsem8@gmail.com>
2025-05-07 15:20:29 +0530

    Add files via upload

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,15 @@
+void  ft_print_alphabet(void)
+{
+  write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+
+int main(void)
+{
+  ft_print_alphabet();
+  return (0);
+}
+
+
+
+//Stole matezzi's discord code because I was too lazy to write my own but i do get the concept just being honest ;)
+
